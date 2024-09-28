fb1cc5817cfc8dc78ff2119fd2215b59bc3ca46d
Raul Santana <ti.raul77@gmail.com>
2024-09-28 17:38:13 -0300

    feat: add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | LIONS" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Home, List } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
 
 export function Header() {
   return (
@@ -20,6 +21,10 @@ export function Header() {
             Serviços
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
 
       </div>
     </div>
